f3071f139943bb6bc181a03667283dffe38ced7f
mattfung99 <official.matthiasfung@gmail.com>
2018-04-10 23:25:01 -0400

    New visuals and sound

--- jsWhackAMole.js
+++ jsWhackAMole.js
@@ -9,6 +9,7 @@ var numberOfSquares = 11;
 var positionChange = 6;
 var randomCoordinateX;
 var randomCoordinateY;
+var soundDelete;
 var squareDivArray = [];
 var startTimer;
 
@@ -17,6 +18,7 @@ var startTimer;
 // ***** ------------------------ **** ------------------------ ***** //
 
 function beginProgram() {
+    document.getElementById("display-timer").innerHTML = "20";
     generateDiv();
     document.getElementById("button-begin").disabled = true;
     document.getElementById("button-begin").innerHTML = "Disabled";
@@ -42,7 +44,7 @@ function beginProgram() {
     }, 1000);
 
     function reset() {
-        document.getElementById("display-timer").innerHTML = "20";
+        document.getElementById("display-timer").innerHTML = "Timer";
         clearInterval(startTimer);
         count = 20;
         clearAll();
@@ -63,9 +65,10 @@ function beginProgram() {
         generateRandomX(min, max);
         generateRandomY(min, max);
 
-        squareDiv = document.createElement("div");
+        squareDiv = document.createElement("img");
+        squareDiv.src = "animatedMole.png";
         squareDiv.id = "squareDiv" + squareDivArray.length;
-        squareDiv.style = "background: rgb(14, 71, 163); border: 1px solid white; cursor: pointer; height: 50px; position: absolute; width: 50px";
+        squareDiv.style = "cursor: pointer; height: 50px; position: absolute";
         squareDiv.style.left = randomCoordinateX + "px";
         squareDiv.style.top = randomCoordinateY + "px";
         squareDiv.addEventListener("click", function () {
@@ -111,6 +114,8 @@ function clearAll() {
 function deleteDivProcess(id) {
     squareDivArray.splice(id.charAt(9), 1);
     container.removeChild(document.getElementById(id));
+    soundDelete = new sound("cough.wav");
+    soundDelete.play();
     for (var j = parseInt(id.charAt(9))+1; j < squareDivArray.length+1; j++) {
         var reassignId = document.getElementById("squareDiv" + j);
         reassignId.id = "squareDiv" + (j-1);
@@ -121,22 +126,29 @@ function deleteDivProcess(id) {
 }
 
 // ***** ------------------------ **** ------------------------ ***** //
-                    // ****** Reset Program ******* //
-// ***** ------------------------ **** ------------------------ ***** //
-
-function reset() {
-    clearAll();
-    generateDiv();
-}
-
-// ***** ------------------------ **** ------------------------ ***** //
                     // ***** DRAW CANVAS TEXT ***** //
 // ***** ------------------------ **** ------------------------ ***** //
 
-var mainMenu = document.getElementById("main-menu");
-var ctxMainMenu = mainMenu.getContext("2d");
+// var mainMenu = document.getElementById("main-menu");
+// var ctxMainMenu = mainMenu.getContext("2d");
 
-ctxMainMenu.font = "20px Arial";
-ctxMainMenu.fillStyle = "rgb(14, 71, 163)";
-ctxMainMenu.textAlign = "center";
-ctxMainMenu.fillText("Main Menu", mainMenu.width / 2, mainMenu.height / 2);
+// ctxMainMenu.font = "20px Arial";
+// ctxMainMenu.fillStyle = "rgb(14, 71, 163)";
+// ctxMainMenu.textAlign = "center";
+// ctxMainMenu.fillText("Main Menu", mainMenu.width / 2, mainMenu.height / 2);
+
+// ***** ------------------------ **** ------------------------ ***** //
+                    // ******** Play Sound ******** //  
+// ***** ------------------------ **** ------------------------ ***** //
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+}
